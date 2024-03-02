0a5e8ffdb450396fc3eb192bc7d8fd629fe436fc
Heather Papp <hpapp@students.chattahoocheetech.edu>
2024-03-02 17:15:27 -0500

    db test

--- Heather/test/WeSports/src/main/java/website/wesports/Business/Customer.java
+++ Heather/test/WeSports/src/main/java/website/wesports/Business/Customer.java
@@ -7,10 +7,17 @@ package website.wesports.Business;
  * Description: Customer Business Object
  * Authors: Heather Papp
  *
- * 
+ *
  *
  */
 
 import java.sql.*;
 public class Customer {
+
+    private String CustID, CustPass, CustUserName;
+    private String CustFirstName, CustLastName, CustAddress, CustEmail;
+    final static String DBDriver = "net.ucanaccess.jdbc.UcanaccessDriver";
+    final static String DBLocation = "jdbc:ucanaccess://C:/Users/heath/OneDrive/Documents/Business/Chatt Tech/JAVA/ChattBank/src/main/resources/ChattBankACCDB.accdb;showSchema=true";
+
+
 }
