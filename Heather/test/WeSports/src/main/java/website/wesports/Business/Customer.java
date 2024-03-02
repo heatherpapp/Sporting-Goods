0c87e6b90bcedec81d57bcea9d9ce849facbc519
Heather Papp <hpapp@students.chattahoocheetech.edu>
2024-03-02 17:09:39 -0500

    test

---
+++ Heather/test/WeSports/src/main/java/website/wesports/Business/Customer.java
@@ -0,0 +1,16 @@
+package website.wesports.Business;
+
+/*
+ * Class: CIST 2931 : Advanced Systems Project Management
+ * Term: Spring 2024
+ * Instructor: Chris Bishop
+ * Description: Customer Business Object
+ * Authors: Heather Papp
+ *
+ * 
+ *
+ */
+
+import java.sql.*;
+public class Customer {
+}
