9bf135a970cd6411884e7527d37d0e491107a709
Heather Papp <hpapp@students.chattahoocheetech.edu>
2024-03-25 20:15:14 -0400

    Create Order.java
    original Orders without the implementation thing, just a copy in case the other doesnt work as i want it to

---
+++ Heather/website2/WeSports/src/main/java/website/wesports/Business/Order.java
@@ -0,0 +1,281 @@
+package website.wesports.Business;
+
+import java.sql.*;
+
+public class Order {
+    /************* Database *************/
+    /**
+     * Database Column Names, Type, and Order
+     * OrderID : autonumber
+     * CustEmail : short text
+     * ProductCode : short text
+     * Quantity : number
+     * OrderDate : Date/Time
+     * OrderStatus : short text
+     * FulfillmentDate : Date/Time
+     * DistUsername : short text
+     */
+    final String DBDriver = "net.ucanaccess.jdbc.UcanaccessDriver";
+    final String DBLocation = "jdbc:ucanaccess://C://WeSportsDB//WeSports.accdb/";
+
+    /************* Properties *************/
+    Long OrderID;
+    int Quantity;
+    String CustEmail, ProductCode, OrderStatus, DistUsername;
+    Timestamp OrderDate; // current date and time order is placed
+    Date FulfillmentDate; // current date only order is fulfilled
+    boolean Exists;
+
+
+    /************* Constructors *************/
+    public Order(String custEmail, String productCode, int quantity, Timestamp orderDate, String orderStatus, Date fulfillmentDate, String distUsername) {
+        this(null, custEmail, productCode, quantity, orderDate, orderStatus, fulfillmentDate, distUsername);
+    }
+
+    public Order(Long orderID, String custEmail, String productCode, int quantity, Timestamp orderDate, String orderStatus, Date fulfillmentDate, String distUsername) {
+        OrderID = orderID;
+        CustEmail = custEmail;
+        ProductCode = productCode;
+        Quantity = quantity;
+        OrderDate = orderDate;
+        OrderStatus = orderStatus;
+        FulfillmentDate = fulfillmentDate;
+        DistUsername = distUsername;
+    }
+
+    /************* Behaviors *************/
+    public void setOrderID(Long orderID) { OrderID = orderID; }
+
+    public Long getOrderID() { return OrderID; }
+
+    public void setCustEmail(String custEmail) { CustEmail = custEmail; }
+    public String getCustEmail() { return CustEmail; }
+
+    public void setProductCode(String productCode) { ProductCode = productCode; }
+    public String getProductCode() { return ProductCode; }
+
+    public void setQuantity(int quantity) { Quantity = quantity; }
+    public int getQuantity() { return Quantity; }
+
+    public void setOrderDate(Timestamp orderDate) { OrderDate = orderDate; }
+    public Timestamp getOrderDate() { return OrderDate; }
+
+    public void setOrderStatus(String orderStatus) { OrderStatus = orderStatus; }
+    public String getOrderStatus() { return OrderStatus; }
+
+    public void setFulfillmentDate(Date fulfillmentDate) { FulfillmentDate = fulfillmentDate; }
+    public Date getFulfillmentDate() { return FulfillmentDate; }
+
+    public void setDistUsername(String distUsername) { DistUsername = distUsername; }
+    public String getDistUsername() { return DistUsername; }
+    //public void setExists(boolean exists) { Exists = exists; }
+    //public boolean getExists() { return  Exists; }
+
+    /*************Check if Order Record Exists *************/
+    public boolean orderExists(Long oid, Connection connection) {
+        Exists = false;
+        try {
+            // Create SQL statement & string
+            Statement stmt = connection.createStatement();
+            String sql = "SELECT * FROM Orders WHERE OrderID = '" + getOrderID() + "'";
+
+            // Execute SQL Query
+            ResultSet rs = stmt.executeQuery(sql);
+
+            // Check if CustID record exists
+            if (rs.next()) {
+                if (rs.getInt(1) != 0) Exists = true;
+                System.out.println("Customer ID: " + oid + " Exists");
+            }
+        } catch (Exception e) {
+            System.out.println("Exception: " + e);
+        }
+        // Return boolean result of recordExists()
+        return Exists;
+    } // END orderExists
+
+
+    /************* Display Results *************/
+    public void display() {
+        if (OrderID == 0) System.out.println("!!!!! You must enter an OrderID !!!!!");
+        System.out.println("=======================================================");
+        System.out.println("OrderID\tCustEmail\t\t\tProductCode\tQuantity\tOrderDate\tOrderStatus\tFulfillmentDate\tDistUsername\n");
+        /**
+         * each OrderID may have multiple entries, due to each row in orderID representing one item set in an order
+         * perhaps a foreach loop here to show column headers above, then loop through each order?
+         */
+        System.out.println(getOrderID() + getCustEmail() + getProductCode() + getQuantity() + getOrderDate() + getOrderStatus() + getFulfillmentDate() + getDistUsername());
+        System.out.println("=======================================================\n");
+    }
+
+    /************* SELECT All Orders from Database: Orders : ORDER BY OrderID *************/
+    //All orders, sorted by OrderID
+    public void selectAllOrders() {
+        try {
+            // Get connection to database
+            Class.forName(DBDriver);
+            Connection connection = DriverManager.getConnection(DBLocation);
+            System.out.println("Database Connected");
+
+            //Create SQL statement & string
+            Statement stmt = connection.createStatement();
+            String sql = "SELECT * FROM Orders ORDER BY OrderID";
+
+            // Execute SQL Query
+            ResultSet rs = stmt.executeQuery(sql);
+            System.out.println("SQL Query: " + sql);
+
+            // Info to retrieve
+            rs.next();
+
+            setOrderID(rs.getLong("OrderID"));
+            setCustEmail(rs.getString("CustEmail"));
+            setProductCode(rs.getString("ProductCode"));
+            setQuantity(rs.getInt("Quantity"));
+            setOrderDate(rs.getTimestamp("OrderDate"));
+            setOrderStatus(rs.getString("OrderStatus"));
+            setFulfillmentDate(rs.getDate("FulfillmentDate"));
+            setDistUsername(rs.getString("DistUsername"));
+
+            // Close Connection
+            connection.close();
+        } catch (Exception e) {
+            System.out.println("Exception" + e);
+        }
+    } //END selectAllOrders
+
+    /************* SELECT All Open Orders from Database: Orders *************/
+    // OrderStatus = Open
+    public void selectOpenOrders() {
+        try {
+            // Get connection to database
+            Class.forName(DBDriver);
+            Connection connection = DriverManager.getConnection(DBLocation);
+            //System.out.println("Database Connected");
+
+            //Create SQL statement & string
+            Statement stmt = connection.createStatement();
+            String sql = "SELECT * FROM Orders WHERE OrderStatus=Open";
+
+            // Execute SQL Query
+            ResultSet rs = stmt.executeQuery(sql);
+            System.out.println("SQL Query: " + sql);
+
+            // Info to retrieve
+            rs.next();
+
+            setOrderID(rs.getLong("OrderID"));
+            setCustEmail(rs.getString("CustEmail"));
+            setProductCode(rs.getString("ProductCode"));
+            setQuantity(rs.getInt("Quantity"));
+            setOrderDate(rs.getTimestamp("OrderDate"));
+            setOrderStatus(rs.getString("OrderStatus"));
+            setFulfillmentDate(rs.getDate("FulfillmentDate"));
+            setDistUsername(rs.getString("DistUsername"));
+
+            // Close Connection
+            connection.close();
+        } catch (Exception e) {
+            System.out.println("Exception" + e);
+        }
+    } //END selectPendingOrders
+
+
+    /************* SELECT All Shipped Orders from Database: Orders *************/
+    // OrderStatus = Shipped
+    public void selectShippedOrders() {
+        try {
+            // Get connection to database
+            Class.forName(DBDriver);
+            Connection connection = DriverManager.getConnection(DBLocation);
+            //System.out.println("Database Connected");
+
+            //Create SQL statement & string
+            Statement stmt = connection.createStatement();
+            String sql = "SELECT * FROM Orders WHERE OrderStatus=Shipped";
+
+            // Execute SQL Query
+            ResultSet rs = stmt.executeQuery(sql);
+            System.out.println("SQL Query: " + sql);
+
+            // Info to retrieve
+            rs.next();
+
+            setOrderID(rs.getLong("OrderID"));
+            setCustEmail(rs.getString("CustEmail"));
+            setProductCode(rs.getString("ProductCode"));
+            setQuantity(rs.getInt("Quantity"));
+            setOrderDate(rs.getTimestamp("OrderDate"));
+            setOrderStatus(rs.getString("OrderStatus"));
+            setFulfillmentDate(rs.getDate("FulfillmentDate"));
+            setDistUsername(rs.getString("DistUsername"));
+
+            // Close Connection
+            connection.close();
+        } catch (Exception e) {
+            System.out.println("Exception" + e);
+        }
+    } //END selectCompleteOrders
+
+
+    /************* INSERT New Order into Database: Orders *************/
+    public void insertNewOrder(Long orderID, String custEmail, String productCode, int quantity, Timestamp orderDate, String orderStatus, Date fulfillmentDate, String distUsername) {
+        setOrderID(orderID);
+        setCustEmail(custEmail);
+        setProductCode(productCode);
+        setQuantity(quantity);
+        setOrderDate(orderDate);
+        setOrderStatus(orderStatus);
+        setFulfillmentDate(fulfillmentDate);
+        setDistUsername(distUsername);
+
+        try {
+            // Get connection to database
+            Class.forName(DBDriver);
+            Connection connection = DriverManager.getConnection(DBLocation);
+            System.out.println("Database Connected");
+
+            // Check if CustID record exists
+            if (!orderExists(orderID, connection)) {
+                // Create SQL string
+                //String sql = "INSERT INTO Orders(OrderID, CustEmail, ProductCode, Quantity, OrderDate, OrderStatus, FulfillmentDate, DistUsername) Values(?,?,?,?,?,?,?,?)";
+                String sql = "INSERT INTO Orders(CustEmail, ProductCode, Quantity, OrderDate, OrderStatus, FulfillmentDate, DistUsername) Values(?,?,?,?,?,?,?)";
+
+                // Prepare SQL statement
+                PreparedStatement pStmt = connection.prepareStatement(sql);
+                System.out.println("SQL Statement: " + sql);
+
+                // Info to insert
+                //pStmt.setInt(1, null); //autonumber, set empty
+                pStmt.setString(2, CustEmail); //get from Cart
+                pStmt.setString(3, ProductCode); //get from Cart
+                pStmt.setInt(4, Quantity); //get from Cart
+                pStmt.setTimestamp(5, OrderDate); //set this to current Date & Time in ddmmyyyy 00:00 format
+                pStmt.setString(6, "Open"); //set to OPEN, this is a new order
+                pStmt.setDate(7, FulfillmentDate); //set empty, order is OPEN
+                pStmt.setString(8, ""); //set empty, order is OPEN, no one has SHIPPED yet
+
+                // Execute SQL Statement & Do Insert
+                int n = pStmt.executeUpdate();
+
+                // Verify Insert
+                if (n == 1) System.out.println("..... INSERT Successful! .....");
+                else System.out.println("***** INSERT FAILED! *****");
+            } else
+                System.out.println("***** ERROR! ***** Cannot Insert NEW Customer! *****\n***** Customer Email: " + custEmail + " already exists! *****");
+
+            // Close connnection
+            connection.close();
+        } catch (Exception e) {
+            System.out.println("Exception" + e);
+        }
+    } //END insertNewOrder
+
+    /**
+     * NO UPDATE --- Orders placed are not to be modified --- Cancel/Delete ONLY
+     */
+
+    /************* DELETE Selected Order from Database: Orders *************/
+    public void deleteOrder() {
+    }
+}
